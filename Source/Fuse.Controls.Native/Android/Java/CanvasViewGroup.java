38a11ceed5ce9ab636843841868354d0865f6e78
Vegard Strand Lende <lende.vegard@gmail.com>
2017-03-30 13:35:38 +0200

    Android: Add CanvasViewGroup

---
+++ Source/Fuse.Controls.Native/Android/Java/CanvasViewGroup.java
@@ -0,0 +1,27 @@
+package com.fuse.android.views;
+
+public class CanvasViewGroup extends ViewGroup {
+
+	public interface DrawListener {
+		void onDraw(android.graphics.Canvas canvas);
+	}
+
+	public CanvasViewGroup(android.content.Context context)
+	{
+		super(context);
+		setWillNotDraw(false);
+	}
+
+	DrawListener _drawListener;
+
+	public void setDrawListener(DrawListener drawListener) {
+		_drawListener = drawListener;
+	}
+
+	@Override
+	protected void onDraw(android.graphics.Canvas canvas) {
+		if (_drawListener != null) {
+			_drawListener.onDraw(canvas);
+		}
+	}
+}
