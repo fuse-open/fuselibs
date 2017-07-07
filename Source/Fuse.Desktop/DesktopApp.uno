fuse-open/fuselibs

commit 81eae3e47960ee0624de073d5d702005b0de9e0e

Move IncreaseFrameIndex to prior to Update to make it consistent across platforms
Because rendering happens independently of Update on desktop, desktop would get inconsistent FrameIndex where you would update with frame N, and render with frame N+1. This messes up cache heuristics etc. and causes slow and jittery rendering on desktop.

Source/Fuse.Nodes/AppBase.uno
@@ -281,11 +281,12 @@ namespace Fuse
 			be overridden in user code. Use @UpdateManager instead. */
 		protected virtual void OnUpdate()
 		{
+			UpdateManager.IncreaseFrameIndex();
+
 			if defined(FUSELIBS_PROFILING)
 				Profiling.BeginUpdate();
 
 			UpdateManager.Update();
-			UpdateManager.IncreaseFrameIndex();
 
 			if defined(FUSELIBS_PROFILING)
 				Profiling.EndUpdate();

commit ed20a04e66ca57a60c86c384b8bf406cfa5448df

make sure IncreaseFrameIndex happens right after draw
Doing things like InvalidateVisual in response to input requires that
the UpdateManager.FrameIndex was increased right after drawing, because
otherwise the frame index will be the same when the input happens,
causing elements that were validated the same frame to not be
invalidated.

This problem was introduced in 81eae3e, where another similar issue
was worked around. This should fix both cases.

Fixes issue #182

Source/Fuse.Desktop/DesktopApp.uno
@@ -66,6 +66,8 @@ namespace Fuse
 
 			if defined(FUSELIBS_PROFILING)
 				Profiling.EndDraw();
+
+			UpdateManager.IncreaseFrameIndex();
 		}
 
 		public override bool NeedsRedraw 

Source/Fuse.Nodes/AppBase.uno
@@ -281,12 +281,13 @@ namespace Fuse
 			be overridden in user code. Use @UpdateManager instead. */
 		protected virtual void OnUpdate()
 		{
-			UpdateManager.IncreaseFrameIndex();
-
 			if defined(FUSELIBS_PROFILING)
 				Profiling.BeginUpdate();
 
 			UpdateManager.Update();
+
+			if defined(MOBILE)
+				UpdateManager.IncreaseFrameIndex();
 
 			if defined(FUSELIBS_PROFILING)
 				Profiling.EndUpdate();