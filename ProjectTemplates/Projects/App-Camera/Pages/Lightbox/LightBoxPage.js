22bed7c77bdd8eae29460bf011c7c86b5c3d4d7a
Andreas Rønning <andreas@doomsday.no>
2017-07-11 14:57:29 +0200

    Minor cleanup

--- ProjectTemplates/Projects/App-Camera/Pages/Lightbox/LightBoxPage.js
+++ ProjectTemplates/Projects/App-Camera/Pages/Lightbox/LightBoxPage.js
@@ -3,7 +3,6 @@ var images = App.getPictures();
 module.exports = {
 	images : images,
 	currentIndex : this.Parameter.map(function(param){
-		var idx = images.length;
 		for(var i=0;i<images.length;i++){
 			if(images.getAt(i).id == param.id){
 				return i;
