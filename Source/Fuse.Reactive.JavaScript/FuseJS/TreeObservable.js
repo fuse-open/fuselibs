c35b4749920bb0be6be786b5247d2f8f2cfbf261
Duckers <anders@fusetools.com>
2017-07-17 16:03:21 +0200

    Make TreeObservable API static to allow changing implementation details later

--- Source/Fuse.Reactive.JavaScript/FuseJS/TreeObservable.js
+++ Source/Fuse.Reactive.JavaScript/FuseJS/TreeObservable.js
@@ -3,14 +3,30 @@
 /* 
 	This function represents the low-level interface between the TreeObservable mechanism
 	on the Uno side, and JS. When an instance of TreeObservable is found in the data context,
-	the Uno side injects entrypoints to the object to allow change callbacks to Uno, 
-	as seen i Fuse.Reactive.TreeObservable.Subscribe.
+	the Uno side instruments the object to allow change callbacks to Uno.
 
-	This class doesn't offer any public APIs. Users are intended to use subclasses that
-	offer a more JS-friendly API, like FuseJS/Store
+	This class offers a set of static methods as public API for manipulating the datacontext.
+	JS code should use this API instead of the instrumented entrypoints directly, as those
+	are subject to change.
  */
 function TreeObservable()
 {
+}
+
+TreeObservable.set = function() {
+	if (this.$set instanceof Function) { this.$set.apply(this, arguments); }
+}
+
+TreeObservable.add = function() {
+	if (this.$add instanceof Function) { this.$add.apply(this, arguments); }
+}
+
+TreeObservable.insertAt = function() {
+	if (this.$insertAt instanceof Function) { this.$insertAt.apply(this, arguments); }
+}
+
+TreeObservable.removeAt = function() {
+	if (this.$removeAt instanceof Function) { this.$set.apply(this, arguments); }
 }
 
 module.exports = TreeObservable;
