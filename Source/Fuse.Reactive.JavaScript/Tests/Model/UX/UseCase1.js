84a62e8f15687a44140625b3fd88e002e85e084a
Duckers <anders@fusetools.com>
2017-08-07 15:54:40 +0200

    Remove console.logs from tests

--- Source/Fuse.Reactive.JavaScript/Tests/Model/UX/UseCase1.js
+++ Source/Fuse.Reactive.JavaScript/Tests/Model/UX/UseCase1.js
@@ -11,16 +11,10 @@ class UseCase1 {
 	}
 
 	add(args) {
-		console.log(this.items[1].$id + " : " + this.items[1].name)
-		console.log(args.data.$id + " : " + args.data.name)
 		this.sel.push(args.data)
 	}
 	
 	remove(args) {
-		console.log(this.items[4].$id + " : " + this.items[4].name)
-		console.log(args.data.$id +  " : " + args.data.name )
-		
-		console.log( this.sel.indexOf(args.data) )
 		this.sel.splice( this.sel.indexOf(args.data), 1 )
 	}
 }
