875687e0f521d857c1ca7b8fb05777f3743af983
Vegard Strand Lende <lende.vegard@gmail.com>
2018-02-27 15:59:10 +0100

    Android: fix horizontal scrollview
    Horizontal scrollview was not implemented on android and for good
    reason. Its not easy to change the scroll direction on android. Had to
    write this nasty code to make it work out.
    
    Fixes https://github.com/fusetools/fuselibs-public/issues/1010

--- Source/Fuse.Controls.Native/Android/Java/HorizontalScrollView.java
+++ Source/Fuse.Controls.Native/Android/Java/HorizontalScrollView.java
@@ -1,32 +1,21 @@
 package com.fuse.android.views;
 
 public class HorizontalScrollView extends android.widget.HorizontalScrollView {
-	
-	public HorizontalScrollView(android.content.Context context)
-	{
+
+	public HorizontalScrollView(android.content.Context context) {
 		super(context);
-		_scroll = new java.util.ArrayList<IScroll>();
 	}
 
-	public void SetIScroll(IScroll scroll)
-	{
-		_scroll.add(scroll);
+	ScrollEventHandler _scrollEventHandler;
+
+	public void setScrollEventHandler(ScrollEventHandler scrollEventhandler) {
+		_scrollEventHandler = scrollEventhandler;
 	}
 
-	public  void RemoveIScroll(IScroll scroll)
-	{
-		_scroll.remove(scroll);
-	}
-
-	java.util.ArrayList<IScroll> _scroll;
-
-	protected void onScrollChanged(int l, int t, int oldl, int oldt)
-	{
-		for (int i = 0; i < _scroll.size(); i++)
-		{
-			_scroll.get(i).OnScrollChanged(l, t, oldl, oldt);
+	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+		if (_scrollEventHandler != null) {
+			_scrollEventHandler.onScrollChanged(l, t, oldl, oldt);
 		}
-
 		super.onScrollChanged(l, t, oldl, oldt);
 	}
 }
