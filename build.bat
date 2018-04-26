91cdf187be6218a3f01e0bfa0af12c546672eb91
Erik Faye-Lund <erik@fusetools.com>
2018-04-26 12:14:12 +0200

    use uno from NuGet instead of Stuff
    To depend less on proprietary infrastructure, let's move uno to a
    NuGet stream instead of a Stuff source.
    
    This also upgrades uno, which is the reason for the previous patch.

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 pushd "%~dp0"
 
+nuget install -ConfigFile Stuff\NuGet.config -OutputDirectory Stuff -ExcludeVersion Stuff\packages.config || goto ERROR
 Stuff\stuff install Stuff || goto ERROR
 Stuff\uno doctor %* || goto ERROR
 
