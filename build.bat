2240b73540953ab696fe6c64048e9c6a9aee8a60
Morten Daniel Fornes <mdfornes@gmail.com>
2018-10-18 22:55:42 +0700

    build.bat: drop 'stuff install'
    There are no longer any stuff files to install.

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@
 pushd "%~dp0"
 
 nuget install -ConfigFile Stuff\NuGet.config -OutputDirectory Stuff -ExcludeVersion Stuff\packages.config || goto ERROR
-Stuff\stuff install Stuff || goto ERROR
 Stuff\uno doctor %* || goto ERROR
 
 popd && exit /b 0
